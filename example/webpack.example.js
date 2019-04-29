263fa4d7cb25d249424dd4109c2ba246628b94fa
Ihor Burlachenko <ihor.burlachenko@gmail.com>
2019-04-29 13:33:10 +0200

    Added example and configs

---
+++ example/webpack.example.js
@@ -0,0 +1,72 @@
+/* eslint-disable */
+const path = require('path');
+const webpackExample = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin/dist/clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const paths = {
+    app: path.resolve(__dirname, '.'),
+    entry: path.resolve(__dirname, './index'),
+    lib: path.resolve(__dirname, '../index'),
+    template: path.resolve(__dirname, './index.html'),
+    output: path.resolve(__dirname, './build'),
+    bundle: path.resolve(__dirname, './build/js/*'),
+};
+
+module.exports = {
+    mode: 'development',
+    entry: paths.entry,
+    output: {
+        path: paths.output,
+        publicPath: '/',
+        filename: './js/[name].[hash].js',
+    },
+    devServer: {
+        port: 8000,
+        contentBase: paths.output,
+        publicPath: '/',
+        overlay: true,
+        compress: true,
+        historyApiFallback: true,
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+        symlinks: true,
+        alias: {
+            'react-dom': '@hot-loader/react-dom'
+        }
+    },
+    module: {
+        rules: [
+            {
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true,
+                        babelrc: false,
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react',
+                            '@babel/preset-typescript',
+                        ],
+                        plugins: [
+                            'react-hot-loader/babel',
+                            '@babel/plugin-proposal-class-properties',
+                        ],
+                    },
+                },
+                test: /\.(tsx?)|(jsx?)$/,
+                include: [paths.app, paths.lib],
+            },
+
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: paths.template,
+        }),
+        new CleanWebpackPlugin(),
+        new webpackExample.HotModuleReplacementPlugin(),
+    ],
+};
---
+++ webpack.dist.js
@@ -0,0 +1,49 @@
+/* eslint-disable */
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const paths = {
+    lib: path.resolve(__dirname, '.'),
+    entry: path.resolve(__dirname, './index'),
+    output: path.resolve(__dirname, './dist'),
+};
+
+module.exports = {
+    mode: 'production',
+    entry: paths.entry,
+    output: {
+        filename: 'index.js',
+        path: paths.output,
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+        symlinks: true,
+    },
+    module: {
+        rules: [
+            {
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true,
+                        babelrc: false,
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react',
+                            '@babel/preset-typescript',
+                        ],
+                        plugins: [
+                            '@babel/plugin-proposal-class-properties',
+                        ],
+                    },
+                },
+                test: /\.(tsx?)|(jsx?)$/,
+                include: [paths.lib],
+            },
+
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+    ],
+};
